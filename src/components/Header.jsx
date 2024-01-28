3ad6f20d77964773ee9c82058ffaaae010a68302
Muchangi001 <andrewmuchangi254@gmail.com>
2024-01-28 15:41:39 +0300

    Sixth commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ import {Link} from "react-router-dom";
 const Header=()=>{
 return(
       <nav className="flex justify-around my-3 ">
-        <Link to="/movies" className="text-white  hover:underline ml-0">Movies</Link>
-        <Link to="/tv" className="text-white  hover:underline transition-all mx-10">TV</Link>
-        <Link to="https://developer.themoviedb.org/reference/tv-series-latest-id" className="text-white  hover:underline transition-all mr-0">Source</Link>
+        <Link to="/movies" className="text-white active:underline  hover:underline ml-0">Movies</Link>
+        <Link to="/tv" className="text-white  hover:underline active:underline transition-all mx-10">TV</Link>
+        <Link to="https://developer.themoviedb.org/reference/tv-series-latest-id" className="text-white  hover:underline active:underline transition-all mr-0">Source</Link>
       </nav>
 )
 }
